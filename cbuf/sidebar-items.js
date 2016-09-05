c19820207d8c12140e39ad4a08809e10be9e0402
Dawid Ciężarkiewicz <dpc@dpc.pw>
2016-09-04 21:54:05 -0800

    Update documentation

---
+++ cbuf/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"struct":[["CBuf","Circular Buffer"],["CBufControl","Circular Buffer Control"]]});
---
+++ implementors/core/fmt/trait.Debug.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["cbuf"] = ["impl&lt;'a,&nbsp;T:&nbsp;<a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> + 'a&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='struct' href='cbuf/struct.CBuf.html' title='cbuf::CBuf'>CBuf</a>&lt;'a,&nbsp;T&gt;","impl&lt;T:&nbsp;<a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a>&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='struct' href='cbuf/struct.CBufControl.html' title='cbuf::CBufControl'>CBufControl</a>&lt;T&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
